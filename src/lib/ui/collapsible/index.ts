f5675422d235e2ef956bdca1216ab8bbe9c74139
Miquel de Domingo <miquelddg@gmail.com>
2025-07-14 17:07:38 +0200

    feat: add collapsible component

---
+++ src/lib/ui/collapsible/index.ts
@@ -0,0 +1,9 @@
+import Content from './collapsible-content.svelte';
+import Trigger from './collapsible-trigger.svelte';
+import Root from './collapsible.svelte';
+
+export const Collapsible = {
+  Content,
+  Root,
+  Trigger
+};
